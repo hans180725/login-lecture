d763f4ff5e1391dab308e2b0021e46ae215d6d54
Han YingRi <61380506+hans180725@users.noreply.github.com>
2021-07-12 14:53:57 +0800

    L18

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -9,9 +9,29 @@ const output = {
   },
 };
 
+const users = {
+  id: ["1", "2", "3"],
+  password: ["11", "22", "33"],
+}
+
 const process = {
   login: (req, res) => {
-    console.log(req.body);
+    const id = req.body.id;
+    const password = req.body.password;
+
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.password[idx] === password) {
+        return res.json({
+          success: true,
+        });
+      }
+    }
+
+    return res.json({
+      success: false,
+      msg: "Login failed!"
+    });
   },
 };
 
