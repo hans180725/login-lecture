b8e3fa1e616f8b4c3917eef7723d560f4fb22d17
Han YingRi <61380506+hans180725@users.noreply.github.com>
2021-07-15 10:37:32 +0800

    L36

--- app/app.js
+++ app/app.js
@@ -3,9 +3,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-dotenv.config();
+const morgan = require("morgan");
 
 const app = express();
+dotenv.config();
+
+const accessLogStream = require("./src/config/log");
 
 const home = require("./src/routes/home");
 
@@ -14,6 +17,8 @@ app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan("dev"));
+app.use(morgan("common", { stream: accessLogStream }));
 app.use("/", home);
 
 module.exports = app;
